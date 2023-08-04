917b77c1686049d623590638a9bf0c54e7ddb148
seobkeom <ajm153123@naver.com>
2023-08-04 15:41:55 +0900

    Board End

--- board/src/main/java/backend/config/DatabaseConfig.java
+++ board/src/main/java/backend/config/DatabaseConfig.java
@@ -5,7 +5,6 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -15,12 +14,14 @@ import org.springframework.context.annotation.PropertySource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @PropertySource("classpath:/application.properties")
+@RequiredArgsConstructor
 public class DatabaseConfig {
 
-    @Autowired
-    private ApplicationContext context;
+    private final ApplicationContext context;
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
@@ -37,20 +38,13 @@ public class DatabaseConfig {
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource());
-        factoryBean.setMapperLocations(context.getResources("classpath:/mappers/**/*Mapper.xml"));
-        factoryBean.setConfiguration(mybatisConfig());
+         factoryBean.setMapperLocations(context.getResources("classpath:/mappers/**/*Mapper.xml"));
         return factoryBean.getObject();
     }
 
     @Bean
     public SqlSessionTemplate sqlSession() throws Exception {
         return new SqlSessionTemplate(sqlSessionFactory());
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "mybatis.configuration")
-    public org.apache.ibatis.session.Configuration mybatisConfig() {
-        return new org.apache.ibatis.session.Configuration();
     }
 
 }
